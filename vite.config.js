c2a26b28702dce55532398d653025636174a36d6
kuba86952@wp.pl <kuba86952@wp.pl>
2024-05-28 22:59:12 +0200

    exercises resolved

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = "";
+const exercisePath = "05_Dzien_3/04_Flexbox/01_Verti";
 
 /**
  * Don't change those lines below
